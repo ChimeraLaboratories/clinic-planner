c3faf4df26cb5a93c24f1ec7f34a2db0c0868021
CaneEmm <c.a.emmingham@icloud.com>
2026-06-05 14:05:19 +0100

    Started developing Settings Page for User Preferences

---
+++ src/app/planner/settings/components/SettingsForm.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+type UserPreferences = {
+    default_calendar_view: string;
+    week_start_day: string;
+    theme: string;
+    compact_view: boolean;
+    show_weekends: boolean;
+}
---
+++ src/app/planner/settings/page.tsx
@@ -0,0 +1,14 @@
+export default function SettingsPage() {
+    return(
+        <div className="space-y-6">
+            <div>
+                <h1 className=text-2x1 font-bold>Settings</h1>
+                <p className="text-slate-500">
+                    Manage your personal preferences.
+                </p>
+            </div>
+
+            {/*Settings card goes here*/}
+        </div>
+    )
+}
